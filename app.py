c68338c44803343cc27f68dd317cfe3756121194
jakerinahmed <jakerin.ahmed2001@gmail.com>
2022-10-18 16:25:55 +0100

    revert to last working copy

--- app.py
+++ app.py
@@ -3,28 +3,28 @@ from flask_cors import CORS
 from werkzeug.exceptions import BadRequest, NotFound, InternalServerError
 from werkzeug import exceptions
 from controllers import shows
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-db = SQLAlchemy(app)
-db.init_app(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# db = SQLAlchemy(app)
+# db.init_app(app)
 
-with app.app_context():
-    class Shows(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(80), unique=True, nullable=False)
-        seasons = db.Column(db.Integer, nullable=False)
+# with app.app_context():
+#     class Shows(db.Model):
+#         id = db.Column(db.Integer, primary_key=True)
+#         name = db.Column(db.String(80), unique=True, nullable=False)
+#         seasons = db.Column(db.Integer, nullable=False)
 
-    GOT = Shows(
-        name = "Game of Thrones",
-        seasons = 8,
-    )
-    db.session.add(GOT)
-    db.session.commit()
+#     GOT = Shows(
+#         name = "Game of Thrones",
+#         seasons = 8,
+#     )
+#     db.session.add(GOT)
+#     db.session.commit()
 
-    db.create_all()
+#     db.create_all()
 
 @app.route('/')
 def hello():
@@ -34,16 +34,11 @@ def hello():
 @app.route('/shows', methods=["GET", "POST"])
 def shows_handler():
     fns = {
-        "GET":index,
+        "GET":shows.index,
         "POST":shows.create
     }
     resp, code = fns[request.method](request)
     return jsonify(resp), code 
-
-def index(req):
-    return db.session.execute(db.select(Shows))
-    # return [show for show in tv_shows], 200
-
 
 @app.route('/shows/<int:shows_id>', methods=["GET", "PATCH", "PUT", "DELETE"])
 def shows_handler_id(shows_id):
@@ -56,9 +51,6 @@ def shows_handler_id(shows_id):
     resp, code = fns[request.method](request, shows_id)
     return jsonify(resp), code
     
-        
-
-
 @app.errorhandler(NotFound)
 def handle404(err):
     return jsonify({'message': f'oops{err}'}), 404
@@ -70,7 +62,6 @@ def handle_400(err):
 @app.errorhandler(InternalServerError)
 def handle500(err):
     return jsonify({'message': "It's not you, it's us"}), 500
-
 
 if __name__ == "__main__":
     app.run(debug=True)
